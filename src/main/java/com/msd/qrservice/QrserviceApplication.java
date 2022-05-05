6336c5a4f565682dc0e00f9aee0581de90999399
mahen <mahendrasridayarathna@gmail.com>
2022-05-05 21:45:14 +0530

    Init project setup

---
+++ src/main/java/com/msd/qrservice/QrserviceApplication.java
@@ -0,0 +1,13 @@
+package com.msd.qrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msd/qrservice/QrserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msd.qrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
